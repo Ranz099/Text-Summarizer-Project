58a4c188b08b0e5bc243f9c54a11b865e9b16ebd
Ranjan Kumar <aranjan8172@gmail.com>
2024-12-28 08:46:18 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,9 +7,9 @@ with open("README.md", "r", encoding="utf-8") as f:
 __version__ = "0.0.0"
 
 REPO_NAME = "Text-Summarizer-Project"
-AUTHOR_USER_NAME = "AYUSH"
+AUTHOR_USER_NAME = "RANJAN"
 SRC_REPO = "textSummarizer"
-AUTHOR_EMAIL = "ayushkumarshaw770@gmail.com"
+AUTHOR_EMAIL = "aranjan8172@gmail.com"
 
 
 
